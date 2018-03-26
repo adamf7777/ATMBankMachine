f71ac4f5af8fc29f1ce08d1fddca71555bbd74a5
adamf7777 <adamf7777@users.noreply.github.com>
2018-03-26 14:41:16 +0100

    Main file updated - AF

--- ATMBankMachine/main.cpp
+++ ATMBankMachine/main.cpp
@@ -8,11 +8,17 @@ int main(void);
 int main(void)
 {
 	int userInput;
+	int mainLoop = 0;
+	char anotherService = 'Y';
 	float withdrawAmount;
 	float userBalance = 642;
 	float machineBalance = 10000;
 
+
+
 	cout << "Welcome to Bank of Ulster \n\n";
+	
+	while (anotherService == 'Y') {
 	cout << "Please select one of the options below:\n";
 
 	cout << "[1] Cash Withdrawal\n";
@@ -21,62 +27,75 @@ int main(void)
 	cout << "[4] Admin Access \n";
 
 	cin >> userInput;
+	
+	
+
+		switch (userInput) {
+		case 1:
+			cout << "Cash Withdrawl Menu\n\n";
+			cout << "Please enter the amount of money you would like to withdraw: \n";
+			cout << char(156);
 
 
-	switch (userInput) {
-	case 1:
-		cout << "Cash Withdrawl Menu\n\n";
-		cout << "Please enter the amount of money you would like to withdraw in GBP: \n";
+			cin >> withdrawAmount;
+			cout << "\n";
 
+			if ((withdrawAmount <= machineBalance) && (withdrawAmount <= userBalance))
+			{
 
-		cin >> withdrawAmount;
-		//cout << "\n";
+				userBalance = userBalance - withdrawAmount;
+				machineBalance = machineBalance - withdrawAmount;
 
-		if (withdrawAmount < (machineBalance))
-		{
-			cout << "test \n";
-			//userBalance = userBalance - withdrawAmount;
-			//machineBalance = machineBalance - withdrawAmount;
+				cout << "Transaction Approved\n\n";
 
-			cout << "Transaction Approved\n";
+				cout << "Counting your money\n\n";
+				
+				cout << "Please take your money\n\n";
 
-			cout << "Counting your money\n";
-			//Sleep(10000);
-			cout << "Please take your money";
+				cout << "Your new balance is: " << char(156) << userBalance ;
+				cout << "\n\n";
+			}
+
+			else if (withdrawAmount > userBalance)
+			{
+				cout << "Insufficient funds available in your account \n";
+			}
+
+			
+			else if (withdrawAmount > machineBalance)
+			{
+				cout << "The machine has insufficient cash for this transaction\n";
+				cout << "Please try again later \n";
+
+			}
+
+			break;
+
+		case 2: // Display onscreen balance
+			cout << "Your current balance is: " << char(156) << userBalance << endl;
+
+			break;
+
+		case 3: // Allow for cash logdement 
+			cout << "Do nothing 3";
+
+			break;
+
+		case 4: // Admin access 
+			cout << "The user balance is: "<< char(156) << userBalance << endl;
+			cout << "The machine balance is: " << char(156) << machineBalance << endl;
+
 		}
 
-		else if (withdrawAmount >= machineBalance)
-		{
-			cout << "The machine has insufficient funds for this transaction\n";
-			cout << "Please try again later \n";
+		// Any further services around 
+		cout << "Would you like any further services? Y/N:\n";
+		cin >> anotherService;
+		cout << "\n\n";
 
 		}
 
-		else if (withdrawAmount > userBalance)
-		{
-			cout << "Insufficient funds available in your account \n";
-		}
-
-		break;
-
-	case 2:
-		cout << "Do nothing";
-
-		break;
-
-	case 3:
-		cout << "Do nothing 3";
-
-		break;
-
-	case 4:
-		cout << userBalance;
-		cout << machineBalance;
-
+	// If 'N' return card 
+	cout << "Your card is now being returned\n";
 	}
 
-	cout << "Would you like any further services?";
-	cin >> userInput;
 
-
-}
