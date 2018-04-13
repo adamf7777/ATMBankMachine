8f42f558b39825f98632a13ee314f276d3042768
matthewcordner <matthewcordner@MATTHEWCORDNER>
2018-04-13 14:09:26 +0100

    Admin.h file started

--- ATMBankMachine/Admin.h
+++ ATMBankMachine/Admin.h
@@ -1,1 +1,14 @@
-#pragma once
+#ifndef Admin_H_
+#define Admin_H_
+
+/**
+* The admin class is the overseeing class for administration of user's and the machine itself
+*
+* optional, long description, spanning
+* several lines.
+*
+* @author Matthew Cordner <cordner-m1@ulster.ac.uk>
+* @license (url of your license)
+* @copyright Matthew Cordner 2018
+*
+*/#pragma once
