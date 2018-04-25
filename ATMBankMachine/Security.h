baa90ea702941423565ca0974742dbdbc3dc41b8
Christopher <Christopher@CHRIS-LAPTOP>
2018-04-25 12:41:48 +0100

    Start of security header file

--- ATMBankMachine/Security.h
+++ ATMBankMachine/Security.h
@@ -1,1 +1,15 @@
-#pragma once
+#ifndef Security_H_
+#define Security_H_
+
+/**
+* Used to determine security protocols are being met before any transactions occur.
+*
+* optional, long description, spanning
+* several lines.
+*
+* @author	Christopher Hull <Faulkner-c5@ulster.ac.uk>
+* @license https://www.gnu.org/licenses/gpl-3.0.en.html
+* @copyright Christopher Hull 2018
+*
+*/
+
