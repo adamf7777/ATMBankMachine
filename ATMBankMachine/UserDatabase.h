6e71e1f4ec9b623478f8789189b0dc6e90ecb560
adamf7777 <37211546+adamf7777@users.noreply.github.com>
2018-04-25 13:39:31 +0100

    Include functions to set remaining user parameters

--- ATMBankMachine/UserDatabase.h
+++ ATMBankMachine/UserDatabase.h
@@ -4,7 +4,7 @@
 /**
 * Used to read and write information from UserDatabase.
 *
-* optional, long description, spanning
+* 
 * several lines.
 *
 * @author	Adam Fawcett <fawcett-a@ulster.ac.uk>
@@ -24,16 +24,21 @@
 class UserDatabase
 {
 public:
+
+	// declaring function to access the user database
 	UserDatabase(std::string filePath);
 
+	// declaring function to rewrite userdatabase vector array
 	void rewriteUserDatabase();
 
+	// declaring function to get vector for user pincode and the user details
 	std::vector<User> getPinCode;
 	std::vector<User> users;
 
-
+	// declaring function to get all user data
 	std::vector<User> getAllUsers();
 
+	// declaring function to 
 	void addUser(User user);
 
 
