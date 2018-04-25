c5dbf3da754017a9e896bd14ebf884b5eb5e3017
Christopher <Christopher@CHRIS-LAPTOP>
2018-04-25 15:19:12 +0100

    Updates on Security

---
+++ ATMBankMachine/Security.cpp
@@ -0,0 +1,15 @@
+/**
+* Checks Security protocols ensuring the correct user is accessing the correct account
+*
+* @author	Christopher Hull <Faulkner-c5@ulster.ac.uk>
+* @license https://www.gnu.org/licenses/gpl-3.0.en.html
+* @copyright Christopher Hull 2018
+*
+*/
+
+// Include Header Files
+#include "Security.h"
+#include "Security.h"
+#include "User.h"
+
+
