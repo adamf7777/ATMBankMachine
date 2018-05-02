8f90c27d5b3e9d9646e7215993a08bf0f917ef82
Christopher <Christopher@CHRIS-LAPTOP>
2018-05-02 15:03:02 +0100

    comments

--- ATMBankMachine/CashStatus.cpp
+++ ATMBankMachine/CashStatus.cpp
@@ -1,44 +1,45 @@
-
-/**
-* Controls the notes depensed from the system.
-*
-* Provides variables based on the type of notes in the system
-* which can be removed by the user when using the variable cashwithdrawal.
-*
-* @author Mícheál Glass <glass-m@ulster.ac.uk>
-* @license https://www.gnu.org/licenses/gpl-3.0.en.html
-* @copyright Mícheál Glass 2018
-*
-*/
-
-
-#include <string>
-#include <iostream>
-#include <fstream>
-#include "User.h"
-#include "CashStatus.h"
-
-	using namespace std;
-
-
-	void getMachineCashStatus()
-	{
-	
-
-	int twentynotes;
-	int withdrawalamount;
-	int tenamount;
-	int tennotes;
-	int fiveamount;
-	int notesdispensed;
-	int fivenotes;
-
-	//withdrawalAmount for number of notes
-	file.open("twentynotes.txt");
-
-		withdrawalamount = 10;
-		twentynotes = (withdrawalamount / 20);
-		cout << "Number of Twenty pound notes:  " << twentynotes << "\n";
-
-		tenamount = (withdrawalamount - (twentynotes * 20));		tennotes = (tenamount / 10);		cout << "Number of Ten pound notes:  " << tennotes << "\n";		fiveamount = (tenamount - (tennotes * 10));		fivenotes = (fiveamount / 5);		cout << "Number of Five pound notes:  " << fivenotes << "\n";		notesdispensed = (fiveamount - (fivenotes * 5));	//Depoist amount with number of notes recorded
-	}
+///*
+///**
+//* Controls the notes depensed from the system.
+//*
+//* Provides variables based on the type of notes in the system
+//* which can be removed by the user when using the variable cashwithdrawal.
+//*
+//* @author Mícheál Glass <glass-m@ulster.ac.uk>
+//* @license https://www.gnu.org/licenses/gpl-3.0.en.html
+//* @copyright Mícheál Glass 2018
+//*
+//*/
+//
+//
+//#include <string>
+//#include <iostream>
+//#include <fstream>
+//#include "User.h"
+//#include "CashStatus.h"
+//
+//	using namespace std;
+//
+//
+//	void getMachineCashStatus()
+//	{
+//	
+//
+//	int twentynotes;
+//	int withdrawalamount;
+//	int tenamount;
+//	int tennotes;
+//	int fiveamount;
+//	int notesdispensed;
+//	int fivenotes;
+//
+//	//withdrawalAmount for number of notes
+//	file.open("twentynotes.txt");
+//
+//		withdrawalamount = 10;
+//		twentynotes = (withdrawalamount / 20);
+//		cout << "Number of Twenty pound notes:  " << twentynotes << "\n";
+//
+//		tenamount = (withdrawalamount - (twentynotes * 20));//////		tennotes = (tenamount / 10);//		cout << "Number of Ten pound notes:  " << tennotes << "\n";//		fiveamount = (tenamount - (tennotes * 10));//		fivenotes = (fiveamount / 5);//		cout << "Number of Five pound notes:  " << fivenotes << "\n";////		notesdispensed = (fiveamount - (fivenotes * 5));//	//Depoist amount with number of notes recorded//
+//	}
+//	
